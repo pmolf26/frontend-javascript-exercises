f3ae9ef66edbbfee1a4bdc9a2d8054b271ac2c69
pomelia <pomelia@example.com>
2015-04-07 02:43:11 +0000

    Mocha Test plus Debugging Code Assigments

--- 03-debugging-code/00_Reference_Error/challenge.js
+++ 03-debugging-code/00_Reference_Error/challenge.js
@@ -1,3 +1,7 @@
-// module.exports.hello = function(msg) {
-//   return "Hello " + msg;
-// };
+ module.exports.hello = function(msg) {
+   return "Hello " + msg;
+ };
+
+ var hello = function(name) {
+   return "Hello " + name
+ };
