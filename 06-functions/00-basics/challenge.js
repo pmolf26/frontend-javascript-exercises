084cd36668185108cb837a3e178b8ab58af7069d
pomelia <pomelia@example.com>
2015-04-12 14:45:56 +0000

    Add 06 functions

--- 06-functions/00-basics/challenge.js
+++ 06-functions/00-basics/challenge.js
@@ -1,3 +1,7 @@
-module.exports.helloWorld = undefined;
+module.exports.helloWorld = function() {
+    return "Hello World";
+  }
 
-module.exports.multiply = undefined;
+module.exports.multiply = function(number1, number2) {
+  return number1 * number2;
+}
