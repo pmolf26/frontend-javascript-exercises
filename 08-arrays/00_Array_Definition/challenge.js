135f191fc5f5075bfa9c18dfd6d60e6d19bf7a42
pomelia <pomelia@example.com>
2015-04-13 01:23:07 +0000

    Add 08 Arrays

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -1,3 +1,10 @@
-module.exports.newArray = undefined;
+module.exports.newArray = function(one, two, three, four) {
+  var array = [one, two, three, four];
+  return array
+};
 
-module.exports.firstAndLast = undefined;
+module.exports.firstAndLast = function(array) {
+  var lastElementIndex = array.length - 1;
+  var changedArray = [array[0], array[lastElementIndex]];
+  return  changedArray
+};
