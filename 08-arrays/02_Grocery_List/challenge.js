f351cb5cd58edb57dfd7570517ec3a2ffb00e133
pomelia <pomelia@example.com>
2015-04-14 13:23:49 +0000

    08 update, 2 fails left, need to resolve includes

--- 08-arrays/02_Grocery_List/challenge.js
+++ 08-arrays/02_Grocery_List/challenge.js
@@ -1,3 +1,11 @@
-module.exports.addItem = undefined;
+module.exports.addItem = function(item, array) {
+    array.includes(item);
+    array.push(item);
+    return array
+};
 
-module.exports.reverseSortedList = undefined;
+module.exports.reverseSortedList = function(array) {
+  array.sort();
+  array.reverse();
+  return array
+};
