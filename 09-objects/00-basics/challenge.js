ca2eb60abee874b1a5483e4ccc67773b002322db
pomelia <pomelia@example.com>
2015-04-15 16:59:38 +0000

    Add 09 Objects

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,6 +1,22 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(courseTitle, courseDuration, courseStudents) {
+  var course = {
+      title:courseTitle,
+      duration:courseDuration,
+      students:courseStudents
+      }
+  return course
+};
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function(object, newProp, newValue) {
+  var Empty = {};
+  if (object == Empty) {
+    object.newProp = newValue;
+    return object
+  }
+  else {
+    return object
+  }
+};
 
 module.exports.formLetter = undefined;
 
